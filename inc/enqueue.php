4ea58c52ae25f665c964f294a751d79cff2fd99e
dylanjharris <dylansbook@gmail.com>
2015-11-07 06:45:42 -0800

    So far so good

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,6 +6,7 @@ function _djh_scripts() {
   wp_enqueue_style( '_djh-style', get_stylesheet_uri() );
 
   wp_enqueue_style( '_djh-fonts', '//fonts.googleapis.com/css?family=Julius+Sans+One|Open+Sans:400,700,400italic' );
+  wp_enqueue_style( '_djh-fonts2', 'https://fonts.googleapis.com/css?family=Kaushan+Script|Source+Code+Pro|Indie+Flower|Playfair+Display' );
 
   wp_enqueue_script( '_djh-main', get_template_directory_uri() . '/js/main.min.js', array('jquery'), '20151101', true );
 
