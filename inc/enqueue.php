fe2c9531ec76d08d2a8cf364068e83a1d4ed35c2
dylanjharris <dylansbook@gmail.com>
2015-11-10 02:26:30 -0800

    Updating

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -7,7 +7,7 @@ function _djh_scripts() {
 
   wp_enqueue_style( '_djh-fonts', '//fonts.googleapis.com/css?family=Open+Sans:400,700,400italic|Source+Code+Pro|Lobster+Two' );
 
-  wp_enqueue_script( '_djh-main', get_template_directory_uri() . '/js/main.min.js', array('jquery'), '20151101', true );
+  wp_enqueue_script( '_djh-main', get_template_directory_uri() . '/js/main.min.js', array('jquery'), '', true );
 
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
     wp_enqueue_script( 'comment-reply' );
