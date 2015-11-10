8c02c37a1f6052003d8f2d2c329b3bd69539e120
dylanjharris <dylansbook@gmail.com>
2015-11-10 02:56:09 -0800

    Adding fonts

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -5,7 +5,7 @@
 function _djh_scripts() {
   wp_enqueue_style( '_djh-style', get_stylesheet_uri() );
 
-  wp_enqueue_style( '_djh-fonts', '//fonts.googleapis.com/css?family=Open+Sans:400,700,400italic|Source+Code+Pro|Lobster+Two' );
+  // wp_enqueue_style( '_djh-fonts', '//fonts.googleapis.com/css?family=Open+Sans:400,700,400italic|Source+Code+Pro|Lobster+Two' );
 
   wp_enqueue_script( '_djh-main', get_template_directory_uri() . '/js/main.min.js', array('jquery'), '', true );
 
